d80dc781c377543aa09a77444dcbc837efdbaa77
gebi9 <gebi9@hotmail.com>
2021-01-18 18:18:05 +0100

    wtf'n nav bar ok now!.

--- src/m1_components/a0_shared_all/a1_navbar/UI_nav_bar.jsx
+++ src/m1_components/a0_shared_all/a1_navbar/UI_nav_bar.jsx
@@ -5,18 +5,21 @@ import {InView, useInView} from 'react-intersection-observer';
 
 export const UI_nav_bar = (props) => {
     const {ref, inView, entry} = useInView({
-        threshold: 0.6,
+        threshold: 0,
     });
 
     return (
-        <div ref={ref} className={"nav_bar_main_wrapper"}>
-            <div className={(inView ? "top_bar_1" : "top_bar_0")}>
+        <div className={"nav_bar_main_wrapper"}>
+            <div ref={ref}>
+                <div className={(inView ? "top_bar_1" : "top_bar_0")}>
 
+                </div>
             </div>
 
-            <div  className={(inView ? "bottom_bar" : "bottom_bar_fixed")}>
+            <div className={(inView ? "bottom_bar" : "bottom_bar_fixed")}>
                 I stay here.
             </div>
+            {console.log("/////:: ", inView)}
         </div>
     );
 }
--- src/m1_components/a0_shared_all/a2_logo_with_image/UI_logo_with_image.jsx
+++ src/m1_components/a0_shared_all/a2_logo_with_image/UI_logo_with_image.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import "./ST_logo_with_image.scss";
+
+
 export const UI_logo_with_image = () => {
+
     return (
-        <div className={"logo_with_divider_wrapper"}>
+        <div  className={"logo_with_divider_wrapper"}>
            log..
         </div>
     );
