025c6d71dfe44645c9f6d200b171653950e6a7c9
gebi9 <gebi9@hotmail.com>
2021-01-23 13:44:50 +0100

    adjusted couple display flex grow etc. next: ready to implement router

--- src/m1_components/a0_shared_all/a4_body_wrapper/UI_body_wrapper.jsx
+++ src/m1_components/a0_shared_all/a4_body_wrapper/UI_body_wrapper.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./ST_body_wrapper.scss";
+
 export const UI_body_wrapper = () => {
     return (
-        <div className={"body_wrapper"}>
-            body..
+        <div className={"body_outer"}>
+            <div className={"body_wrapper"}>
+                lorem2000
+            </div>
         </div>
     );
 }
